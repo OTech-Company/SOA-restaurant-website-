61730477e3c9ae9cb6e47df4c3102445b4fc3d64
Farah-Tarek101 <farahtarek707@gmail.com>
2024-12-29 04:13:10 +0200

    "added login and registration react frontend"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
-import { useState } from 'react'
 import LoginPopup from './components/LoginPopup/LoginPopup'
-import Verify from './pages/Verify/Verify'
+import Navbar from './components/Navbar/Navbar'
+import Cart from './pages/Cart/Cart'
+import Home from './pages/Home/Home'
 import MyOrders from './pages/MyOrders/MyOrders'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Verify from './pages/Verify/Verify'
+import Login from './pages/Home/login/login' // Adjust path as necessary
+import Register from './pages/Home/Register/Register' // Adjust path as necessary
 
 const App = () => {
-
-  const [showLogin,setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-    <div className='app'>
-        <Navbar setShowLogin={setShowLogin}  />
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
           <Route path='/verify' element={<Verify />} />
           <Route path='/myorders' element={<MyOrders />} />
+          <Route path='/login' element={<Login />} /> {/* Added Login Route */}
+          <Route path='/register' element={<Register />} /> {/* Added Register Route */}
         </Routes>
       </div>
       <Footer />
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import './Header.css'
-import { assets } from '../../assets/assets'
+import React from 'react';
+import './Header.css';
+import headerImage from '../../assets/header_img.png';  // Import the image directly
 
 const Header = () => {
   return (
-    <div className='header'>
-              <img src={assets.header_img} alt="" className='header_img' />
-        <div className="header-contents">
-            <h2>Order your favorite food here</h2>
-            <p>Choose from a diverse menu featuring a delactable array of dishes crafted with the finest ingredients and satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-            <a href="#explore-menu"><button className='buttonwl'>View Menu</button></a>
-        </div>
+    <div className='header' style={{ backgroundImage: `url(${headerImage})` }}>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+        <a href="#explore-menu"><button className='buttonwl'>View Menu</button></a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
